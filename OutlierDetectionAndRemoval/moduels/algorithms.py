3e8a1f68dfd06b1bcb8864a444349641cd1e3d6b
NoamBasha <72918745+NoamBasha@users.noreply.github.com>
2022-03-21 23:44:57 +0200

    Add files via upload

---
+++ OutlierDetectionAndRemoval/moduels/algorithms.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import scipy
+from sklearn.neighbors import KDTree
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class CleaningAlgorithm:
+    def __init__(self, dataset_path, delimiter=None, metric=None, k=None, n=None):
+        """
+        :param dataset_path: the path to the dataset
+        :param delimiter: the delimeter of the dataset
+        :param metric: the metric for computing the distances
+        :param k: number of neighbors to consider
+        :param n: number of outliers to detect
+        """
+        self.dataset_path = dataset_path
+        self.delimiter = delimiter if delimiter else ','
+        self.metric = metric if metric else "euclidean"
+        self.k = k if k else 3
+        self.n = n if n else 10
+        self.outliers_indices = None
+
+    def visualize_outliers(self, outliers, cols_pairs):
+        dtf = pd.read_csv(self.dataset_path, header=None, low_memory=False)
+        for (x, y) in cols_pairs:
+            x_axis = dtf[x]
+            y_axis = dtf[y]
+            x_outliers = dtf.loc[outliers][x]
+            y_outliers = dtf.loc[outliers][y]
+            plt.scatter(x_axis, y_axis, color='b')
+            plt.scatter(x_outliers, y_outliers, color='r')
+            plt.savefig(f'{x}, {y}')
+        
+    def copy_without_outliers(self, to_path):
+        """
+        :param to_path: the path of the new file to copy the dataset without the outliers to
+        """
+        dtf = pd.read_csv(self.dataset_path, header=None, low_memory=False)
+        dtf_without_outliers = dtf.drop(self.outliers_indices)
+        dtf_without_outliers.to_csv(to_path, index=False, header=False)
+        
+        
+class TreeCleaningAlgorithm(CleaningAlgorithm):
+    def get_outliers_indices(self):
+        # Parsing the dataset
+        dtf = np.genfromtxt(self.dataset_path, delimiter=self.delimiter)
+        # Creating a KDTree to store the dataset - letting us find neighbors quickly
+        tree = KDTree(dtf, metric=self.metric)
+        # Querying the tree, finding k-th neighbor to each vector (NOTE: k is k+1)
+        distances, indices = tree.query(dtf, self.k + 1)
+        # Retrieving only the desired column
+        distances_k = distances[:, self.k]
+        # Retrieving the top n outlier indices
+        self.outliers_indices = np.argpartition(distances_k, len(distances_k) - self.n)[-self.n:]
+        # Minus one to all indices
+        # self.outliers_indices = np.subtract(self.outliers_indices, 1)
+        return self.outliers_indices
+
+class MatrixCleaningAlgorithm(CleaningAlgorithm):
+    def get_outliers_indices(self):
+        # Pasring the dataset
+        dtf = pd.read_csv(self.dataset_path, header=None, low_memory=False)
+        # Computing a distance matrix from all vectors to all vectors in the dataset
+        distances = scipy.spatial.distance.cdist(dtf.values, dtf.values,self.metric)
+        # Creating a dataframe from the distances matrix
+        distances_dtf = pd.DataFrame(distances, index=dtf.index, columns=dtf.index)
+        # Sorting the distance matrix by value for each row
+        sorted_distances_dtf = distances_dtf.apply(lambda x: np.sort(x), axis=1, raw=True)
+        # Sorting the sorted distance matrix by the k-th column
+        k_sorted_distances_dtf = sorted_distances_dtf.sort_values([self.k], ascending=False)
+        # Getting the top n outliers indices in the distance matrix based on the distance to their k-th nearest neighbor
+        self.outliers_indices = list(k_sorted_distances_dtf.index)[:self.n]
+        return self.outliers_indices
+    
+def k_mean_data_cleaning(dataset_name, ds):
+    k = 5
+    n = round(len(ds) * 0.1)
+    #print("Dataset size is: {0}, number of outlier isL {1}", len(ds), n)
+    tree = KDTree(ds, metric='euclidean')
+    distances, indices = tree.query(ds, k + 1)
+    distances_k = distances[:, k]
+    outliers_indices = np.argpartition(distances_k, len(distances_k) - n)[-n:]
+    #print(outliers_indices)
+    ds = ds.drop(ds.index[outliers_indices])
+    #clean_ds = np.delete(ds, outliers_indices, axis=0)
+    return ds
---
+++ OutlierDetectionAndRemoval/moduels/parseCSVtoDataset.py
@@ -0,0 +1,124 @@
+import csv
+import time
+import datetime
+import pandas as pd
+
+
+def read_csv_to_df(path, id_col_name, columns):
+    if id_col_name is None:
+        dtf = pd.read_csv(path)
+    else:
+        dtf = pd.read_csv(path, index_col=id_col_name)
+    dtf = dtf[columns]
+    return dtf
+
+def copy_without_header(from_path, to_path):
+    dtf = pd.read_csv(from_path, low_memory=False)
+    dtf.to_csv(to_path, index=False, header=False)
+    
+    
+def write_dataset_to_csv(path, dataset):
+    with open(path, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(dataset)
+
+
+def brazilian_houses_to_data_set(old_path, new_path):
+    brazilian_houses_dataset = list()
+    with open(old_path, encoding='utf-8', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for i, row in enumerate(spamreader):
+            if i == 0:
+                del row[0]
+                """`
+                for j in range(data_binary.shape[1], 0, -1):
+                    row = ["city-{}".format(j)] + row
+                row = ["Id"] + row
+                """
+            else:
+                if row[5] == '-':
+                    row[5] = 0
+                row[6] = (1 if row[6] == 'acept' else 0)
+                row[7] = (1 if row[7] == 'furnished' else 0)
+                # city_ind = brazilian_houses_cities_list.index(row[0])
+                del row[0]
+                # row = data_binary.values[city_ind].tolist() + row
+                # row = [i] + row
+            brazilian_houses_dataset.append(row)
+    write_dataset_to_csv(new_path, brazilian_houses_dataset)
+
+
+def COVID_symptoms_to_data_set(old_path, new_path):
+    covid_symptoms_dataset = list()
+    with open(old_path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for i, row in enumerate(reader):
+            del row[-1]
+            del row[19:22]
+            covid_symptoms_dataset.append(row)
+    write_dataset_to_csv(new_path, covid_symptoms_dataset)
+
+
+def goodreads_books_to_data_set(old_path, new_path):
+    goodreads_books_dataset = list()
+    with open(old_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for i, row in enumerate(reader):
+            if i == 0:
+                author_row = []
+                lancode_row = []
+                publisher_row = []
+                del row[0:3]
+                del row[1:4]
+                del row[-1]
+            else:
+                del row[0:2]
+                del row[0]
+                try:
+                    float(row[0])
+                except:
+                    del row[0]
+                del row[1:4]
+                del row[-1]
+                try:
+                    t = datetime.datetime.strptime(row[-1], "%m/%d/%Y")
+                    if t == datetime.datetime(1970, 1, 1):
+                        row[-1] = 0.0
+                    elif t.year < 1970:
+                        epoch = datetime.datetime(1970, 1, 1)
+                        t = datetime.datetime.strptime(row[-1], "%m/%d/%Y")
+                        diff = t - epoch
+                        row[-1] = diff.total_seconds()
+                    else:
+                        row[-1] = time.mktime(datetime.datetime.strptime(row[-1],
+                                                                         "%m/%d/%Y").timetuple())
+                except Exception as e:
+                    row[-1] = 0.0
+            if i != 0:
+                for r in row:
+                    if r is str:
+                        r = r.replace("\"", "")
+                        r = float(r)
+            goodreads_books_dataset.append(row)
+    write_dataset_to_csv(new_path, goodreads_books_dataset)
+
+
+def Jan_flight_delay_to_data_set(old_path, new_path):
+    jan_flights_dataset = list()
+    with open(old_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for i, row in enumerate(reader):
+            flag = True
+            del row[2:7]
+            del row[3:5]
+            del row[4:6]
+            del row[6]
+            if i != 0:
+                for i in range(len(row)):
+                    if '\"' in row[i]:
+                        row[i] = row[i].replace("\"", "")
+                    if row[i] == '':
+                        flag = False
+            if flag:
+                jan_flights_dataset.append(row)
+    write_dataset_to_csv(new_path, jan_flights_dataset)
